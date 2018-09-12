b60de9f92e355c533c26db1404f251286d308638
244 123 <l.papoyan@mail.rugit config --global user.name LyudvigPapoyan>
2018-09-12 17:21:36 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,15 @@
+const a = 11;
+const b = 22;
+const c = '33';
+const d = '44';
+const e = '77';
+
+if(a === 11){
+	console.log("Correct");
+}
+if(b === 22){
+	console.log("Correct");
+}
+if(e === '33' + '44' - a ){
+	console.log("Incorrect");
+}
